b79e1636577c1e117bb4b0dff665bdb4375f25d3
Duduf15 <cyril.dufay@gmail.com>
2017-01-11 22:41:34 +0100

    ajout tab layout

--- app/src/main/java/com/ufrstgi/imr/application/MainActivity.java
+++ app/src/main/java/com/ufrstgi/imr/application/MainActivity.java
@@ -3,7 +3,12 @@ package com.ufrstgi.imr.application;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +19,9 @@ import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.ufrstgi.imr.application.Fragment.FragmentColis;
+import com.ufrstgi.imr.application.Fragment.FragmentFeuilleRoute;
+import com.ufrstgi.imr.application.Fragment.FragmentNavigation;
 import com.ufrstgi.imr.application.database.local.AdresseManager;
 import com.ufrstgi.imr.application.database.local.CamionManager;
 import com.ufrstgi.imr.application.database.local.ChauffeurManager;
@@ -44,8 +52,7 @@ import com.ufrstgi.imr.application.object.PositionChauffeur;
 import com.ufrstgi.imr.application.object.PositionColis;
 import com.ufrstgi.imr.application.object.Tournee;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -60,6 +67,8 @@ public class MainActivity extends AppCompatActivity
     private ServerHTTP serverHTTP;
     private final static int PORT = 8080;
     List<Colis> mesColis;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,6 +76,16 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -78,96 +97,8 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
 
-        /* Test BDD */
-        Niveau niveau = new Niveau(1,"Niveau 1",150);
-        Personne conducteur_camion = new Personne(1,"Westermann","Thomas","0621065701");
-        Personne contact_client = new Personne(2,"Spies","François","0654865701");
-        Camion camion = new Camion("BK-589-KD","Peugeot 308",25,2,1500);
-        Latlng latlng = new Latlng(1,40,7);
-        Chauffeur chauffeur = new Chauffeur("TW272D8N","password",52,conducteur_camion);
-        Date today = Calendar.getInstance().getTime();
-        Horaire horaire = new Horaire(1,today,today,chauffeur);
-        Adresse adresse_client = new Adresse(1,"6 rue Maurice Ravel",70400,"Hericourt","France",latlng);
-        Adresse adresse_operation = new Adresse(2,"9 rue des Alouettes",90782,"Belfort","France",latlng);
-        PositionChauffeur positionChauffeur = new PositionChauffeur(0,today,chauffeur,latlng);
-        Tournee tournee = new Tournee(1,chauffeur,camion);
-        Client client = new Client(1,"Dufay Cyril","0621065807",adresse_client,contact_client);
-        Livraison livraison = new Livraison(1,today,today,today,"9B","Nodier",adresse_operation, client);
-        Colis colis0 = new Colis(1,1782,0.25f,47,22,80,niveau,livraison,tournee,client);
-        Colis colis1 = new Colis(2,156462,0.25f,47,22,80,niveau,livraison,tournee,client);
-        Colis colis2 = new Colis(3,162,0.25f,47,22,80,niveau,livraison,tournee,client);
-        Colis colis3 = new Colis(4,11435,0.25f,47,22,80,niveau,livraison,tournee,client);
-        PositionColis positionColis = new PositionColis(1,today,colis0,latlng);
 
-        NiveauManager niveauManager = new NiveauManager(this);
-        niveauManager.open();
-        niveauManager.addNiveau(niveau);
-        niveauManager.close();
 
-        PersonneManager personneManager = new PersonneManager(this);
-        personneManager.open();
-        personneManager.addPersonne(conducteur_camion);
-        personneManager.addPersonne(contact_client);
-        personneManager.close();
-
-        CamionManager camionManager = new CamionManager(this);
-        camionManager.open();
-        camionManager.addCamion(camion);
-        camionManager.close();
-
-        LatlngManager latlngManager = new LatlngManager(this);
-        latlngManager.open();
-        latlngManager.addLatlng(latlng);
-        latlngManager.close();
-
-        ChauffeurManager chauffeurManager = new ChauffeurManager(this);
-        chauffeurManager.open();
-        chauffeurManager.addChauffeur(chauffeur);
-        chauffeurManager.close();
-
-        HoraireManager horaireManager = new HoraireManager(this);
-        horaireManager.open();
-        horaireManager.addHoraire(horaire);
-        horaireManager.close();
-
-        AdresseManager adresseManager = new AdresseManager(this);
-        adresseManager.open();
-        adresseManager.addAdresse(adresse_client);
-        adresseManager.addAdresse(adresse_operation);
-        adresseManager.close();
-
-        PositionChauffeurManager positionChauffeurManager = new PositionChauffeurManager(this);
-        positionChauffeurManager.open();
-        positionChauffeurManager.addPositionChauffeur(positionChauffeur);
-        positionChauffeurManager.close();
-
-        TourneeManager tourneeManager = new TourneeManager(this);
-        tourneeManager.open();
-        tourneeManager.addTournee(tournee);
-        tourneeManager.close();
-
-        ClientManager clientManager = new ClientManager(this);
-        clientManager.open();
-        clientManager.addClient(client);
-        clientManager.close();
-
-        OperationManager operationManager = new OperationManager(this);
-        operationManager.open();
-        operationManager.addOperation(livraison);
-        operationManager.close();
-
-        ColisManager colisManager = new ColisManager(this);
-        colisManager.open();
-        colisManager.addColis(colis0);
-        colisManager.addColis(colis1);
-        colisManager.addColis(colis2);
-        colisManager.addColis(colis3);
-        colisManager.close();
-
-        PositionColisManager positionColisManager = new PositionColisManager(this);
-        positionColisManager.open();
-        positionColisManager.addPositionColis(positionColis);
-        positionColisManager.close();
     }
 
     @Override
@@ -182,7 +113,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        // Handle fragment_navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
@@ -242,5 +173,42 @@ public class MainActivity extends AppCompatActivity
     public void onPause() {
         super.onPause();
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FragmentNavigation(), "Navigation");
+        adapter.addFragment(new FragmentColis(), "Colis");
+        adapter.addFragment(new FragmentFeuilleRoute(), "Fuille de route");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
 
 }
